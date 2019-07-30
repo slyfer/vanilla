99fa3a9cf74ff9933222bd0f9f30b87a51fef560
Ciro Cardone <cardone.ciro@gmail.com>
2019-07-30 23:57:34 +0200

    - fix compilation problem - add frontend to maven reactor

--- backend/src/main/java/it/ciro/backend/security/SecurityComponent.java
+++ backend/src/main/java/it/ciro/backend/security/SecurityComponent.java
@@ -1,15 +1,8 @@
 package it.ciro.backend.security;
 
-import com.pkb.business.user.User;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.util.CollectionUtils;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class SecurityComponent {
@@ -20,9 +13,9 @@ public class SecurityComponent {
             res = (UserPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         } catch (ClassCastException cce) {
             res = new UserPrincipal(null,
-                    SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString(),
                     null,
-                    null);
+                    SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString(),
+                    true);
         }
         return res;
     }
@@ -38,22 +31,4 @@ public class SecurityComponent {
     public Authentication getAuthentication() {
         return SecurityContextHolder.getContext().getAuthentication();
     }
-
-    public UserPrincipal toUserPrincipal(User user) {
-        List<GrantedAuthority> authorities = null;
-        if (!CollectionUtils.isEmpty(user.getRoles())) {
-            authorities = user.getRoles().stream().map(role ->
-                    new SimpleGrantedAuthority(role.getName().name())
-            ).collect(Collectors.toList());
-        }
-
-        return new UserPrincipal(
-                user.getTechnicalId(),
-                user.getEmail(),
-                user.getPassword(),
-                authorities
-        );
-    }
-
-
 }
